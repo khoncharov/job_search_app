dff98742c9a5c24c92023868c8d48708c5340a4c
Konstantin Honcharov <k.honcharov@gmail.com>
2023-05-19 01:01:15 +0300

    feat: add card item interface

---
+++ src/types/card-item.ts
@@ -0,0 +1,11 @@
+export interface CardItem {
+  id: number;
+  profession: string;
+  firmName: string;
+  paymentFrom: number;
+  paymentTo: number;
+  currency: string;
+  typeOfWork: string;
+  location: string;
+  description: string;
+}
