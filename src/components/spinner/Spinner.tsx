ad448632cae3aeb258f2a9882f0722386ee0a236
Konstantin Honcharov <k.honcharov@gmail.com>
2023-05-18 19:36:26 +0300

    feat: add spinner component

---
+++ src/components/spinner/Spinner.tsx
@@ -0,0 +1,11 @@
+import './spinner.css';
+
+export const Spinner = () => {
+  return (
+    <div className="spinner">
+      <div className="spinner-mark"></div>
+      <div className="spinner-mark"></div>
+      <div className="spinner-mark"></div>
+    </div>
+  );
+};
