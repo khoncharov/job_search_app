5212b8e07d147bfb25a50d3d02aca4e814bbd3ac
Konstantin Honcharov <k.honcharov@gmail.com>
2023-05-20 01:25:51 +0300

    feat: add favorite list component

--- src/components/jobs-favorite-list/JobsFavoriteList.tsx
+++ src/components/jobs-favorite-list/JobsFavoriteList.tsx
@@ -1,15 +1,18 @@
 import { getVacanciesList } from '../../MOCK_DATA';
+import { getFavoriteItems } from '../../services/favItem';
 import ListItemComponent from '../list-item/ListItem';
 
 const JobsFavoriteListComponent = () => {
-  const vacanciesList = getVacanciesList();
+  const favoriteItems = getFavoriteItems();
+
+  const vacanciesList = getVacanciesList().filter((v) => favoriteItems.includes(v.id));
 
   return (
     <main className="main-list">
-      {/* <ul className="list-container">
-          {vacanciesList.length > 0 &&
-            vacanciesList.map((v) => <ListItemComponent key={v.id} {...v} />)}
-        </ul> */}
+      <ul className="list-container">
+        {vacanciesList.length > 0 &&
+          vacanciesList.map((v) => <ListItemComponent key={v.id} {...v} />)}
+      </ul>
     </main>
   );
 };
--- src/components/jobs-list/JobsList.tsx
+++ src/components/jobs-list/JobsList.tsx
@@ -2,19 +2,22 @@ import { getVacanciesList } from '../../MOCK_DATA';
 import FilterComponent from '../filter/Filter';
 import KeywordInputComponent from '../keyword-input/KeywordInput';
 import ListItemComponent from '../list-item/ListItem';
+import './jobs-list.css';
 
 const JobsListComponent = () => {
   const vacanciesList = getVacanciesList();
 
   return (
     <main className="main-list">
-      <FilterComponent />
-      <div className="list-container">
-        <KeywordInputComponent />
-        <ul className="list-container">
-          {vacanciesList.length > 0 &&
-            vacanciesList.map((v) => <ListItemComponent key={v.id} {...v} />)}
-        </ul>
+      <div className="jobs-list-grid">
+        <FilterComponent />
+        <div className="list-container">
+          <KeywordInputComponent />
+          <ul className="list-container">
+            {vacanciesList.length > 0 &&
+              vacanciesList.map((v) => <ListItemComponent key={v.id} {...v} />)}
+          </ul>
+        </div>
       </div>
     </main>
   );
