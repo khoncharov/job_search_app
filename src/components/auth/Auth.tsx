1c9f57be85edef0120ac79724d3fd4a14bb44062
Konstantin Honcharov <k.honcharov@gmail.com>
2023-05-19 05:18:46 +0300

    feat: add auth component

---
+++ src/components/auth/Auth.tsx
@@ -0,0 +1,12 @@
+import { Spinner } from '../spinner/Spinner';
+import './auth.css';
+
+const AuthComponent = () => {
+  return (
+    <div className="auth-container">
+      <Spinner />
+    </div>
+  );
+};
+
+export default AuthComponent;
