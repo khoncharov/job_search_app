66b007875e1e7c5e98d8fbaf2f0207a71f760652
Konstantin Honcharov <k.honcharov@gmail.com>
2023-05-22 18:10:43 +0300

    fix: add output of external error response

--- src/components/error-page/ErrorPage.tsx
+++ src/components/error-page/ErrorPage.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from 'react';
 import { Link, isRouteErrorResponse, useRouteError } from 'react-router-dom';
+import { CustomErrorResponse } from '../../types/response';
 import './error-page.css';
 
 const ErrorPageComponent = () => {
@@ -6,17 +8,26 @@ const ErrorPageComponent = () => {
 
   let errorStr;
   if (isRouteErrorResponse(error)) {
-    errorStr = `Error ${error.status}: ${error.statusText}`;
+    if (error.internal) {
+      errorStr = `Error ${error.status}: ${error.statusText} [${error.error?.message}]`;
+    } else {
+      const e = error.data as CustomErrorResponse;
+      errorStr = `Error ${e.error.code}: ${e.error.message}`;
+    }
   } else if (error instanceof Error) {
     errorStr = `Error: ${error.message}`;
   } else {
     errorStr = '';
   }
 
+  useEffect(() => {
+    document.title = 'Страница не найдена';
+  }, []);
+
   return (
     <div className="filling-container">
       <p>{errorStr}</p>
-      <Link className="empty-item-btn" to="/">
+      <Link className="empty-item-btn" to="/" reloadDocument>
         Home page
       </Link>
     </div>
