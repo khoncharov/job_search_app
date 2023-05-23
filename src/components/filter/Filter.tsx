f75e5da49a915ed4b19109c40ff009531925303b
Konstantin Honcharov <k.honcharov@gmail.com>
2023-05-23 03:33:53 +0300

    feat: add filter and keyword layout and styling

--- src/components/filter/Filter.tsx
+++ src/components/filter/Filter.tsx
@@ -1,7 +1,52 @@
+import { NumberInput, Select } from '@mantine/core';
+import { IconChevronDown } from '@tabler/icons-react';
+import { getCatalogItems } from '../../services/catalogues-storage';
 import './filter.css';
 
-const FilterComponent = () => {
-  return <form className="filter-container"></form>;
+interface SelectItem {
+  value: string;
+  label: string;
+}
+
+const FilterComponent: React.FC = () => {
+  const catalogues = getCatalogItems();
+  const selectList: SelectItem[] = catalogues.map((i) => ({
+    value: i.key.toString(),
+    label: i.title,
+  }));
+
+  return (
+    <div className="filter-container">
+      <div className="filter-header">
+        <h2>Фильтры</h2>
+        <button type="button" className="filter-reset-btn">
+          Сбросить все
+        </button>
+      </div>
+      <div className="filter-inputs-wrapper">
+        <div className="input-item-container">
+          <label htmlFor="job-filter-select">Отрасль</label>
+          <Select
+            id="job-filter-select"
+            placeholder="Выберете отрасль"
+            data={selectList}
+            radius="0.8rem"
+            size="xl"
+            rightSection={<IconChevronDown size="1.4rem" />}
+            rightSectionWidth={30}
+          />
+        </div>
+        <fieldset>
+          <div className="input-item-container">
+            <legend>Оклад</legend>
+            <NumberInput placeholder="От" radius="0.8rem" size="xl" />
+            <NumberInput placeholder="До" radius="0.8rem" size="xl" />
+          </div>
+        </fieldset>
+        <button className="btn-prime btn-size-m btn-filter-apply">Применить</button>
+      </div>
+    </div>
+  );
 };
 
 export default FilterComponent;
--- src/components/keyword-input/KeywordInput.tsx
+++ src/components/keyword-input/KeywordInput.tsx
@@ -1,19 +1,47 @@
+import { useEffect, useState } from 'react';
 import './keyword-input.css';
 
-const KeywordInputComponent = () => {
+const KeywordInputComponent: React.FC = () => {
+  const [value, setValue] = useState('');
+  const [keyword, setKeyword] = useState('');
+
+  useEffect(() => {
+    console.log(Date.now(), keyword);
+  }, [keyword]);
+
+  const btnHandler = () => {
+    setKeyword(value);
+  };
+
+  const keyDownHandler: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
+    if (e.key === 'Enter') btnHandler();
+  };
+
+  const inputHandler: React.FormEventHandler<HTMLInputElement> = (e) => {
+    setValue(e.currentTarget.value);
+  };
+
   return (
-    <form className="keyword-input__container">
+    <div className="keyword-input__container">
       <input
         className="keyword-input"
         aria-label="Поиск по названию вакансии"
         type="text"
+        name="keyword"
+        value={value}
+        onInput={inputHandler}
         placeholder="Введите название вакансии"
+        onKeyDown={keyDownHandler}
       />
       <div className="keyword-input__icon"></div>
-      <button type="button" className="btn-prime btn-size-s keyword-input__btn">
+      <button
+        type="button"
+        className="btn-prime btn-size-s keyword-input__btn"
+        onClick={btnHandler}
+      >
         Поиск
       </button>
-    </form>
+    </div>
   );
 };
 
