a3c756e15977c0e8be0a9cf759d5598b555b6c6a
Konstantin Honcharov <k.honcharov@gmail.com>
2023-05-23 03:07:08 +0300

    fix: remove title

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,14 +1,9 @@
 import HeaderComponent from '../components/header/Header';
 import { Outlet, useNavigation } from 'react-router-dom';
 import SpinnerComponent from '../components/spinner/Spinner';
-import { useEffect } from 'react';
 
 const RootComponent: React.FC = () => {
   const navigation = useNavigation();
-
-  useEffect(() => {
-    document.title = 'Jobored - Главная';
-  }, []);
 
   return (
     <>
