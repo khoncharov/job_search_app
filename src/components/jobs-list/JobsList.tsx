50c845ef782399a615932f0e61f107dc15649546
Konstantin Honcharov <k.honcharov@gmail.com>
2023-05-23 20:09:42 +0300

    feat: add spinner on loading list data

--- src/components/jobs-list/JobsList.tsx
+++ src/components/jobs-list/JobsList.tsx
@@ -9,6 +9,7 @@ import { getVacancies } from '../../services/api';
 import { VacanciesResponse } from '../../types/response';
 import { mapVacanciesResponse } from '../../services/utils';
 import { SearchState, initState, reducer } from './jobs-list-reducer';
+import SpinnerComponent from '../spinner/Spinner';
 import './jobs-list.css';
 
 const formatPayment = (value: number | '') => {
@@ -20,8 +21,10 @@ const formatPayment = (value: number | '') => {
 
 const updateVacancies = async (
   state: SearchState,
-  setList: React.Dispatch<React.SetStateAction<Vacancy[]>>
+  setList: React.Dispatch<React.SetStateAction<Vacancy[]>>,
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>
 ) => {
+  setLoading(true);
   const tokenInfo = await checkToken();
 
   const response = await getVacancies(tokenInfo.accessToken, {
@@ -37,35 +40,19 @@ const updateVacancies = async (
 
   const data = (await response.json()) as VacanciesResponse;
   setList(mapVacanciesResponse(data));
+  setLoading(false);
 };
 
 const JobsListComponent: React.FC = () => {
   const [state, dispatch] = useReducer(reducer, initState);
+  const [isLoading, setLoading] = useState(true);
 
   const [vacanciesList, setVacanciesList] = useState<Vacancy[]>([]);
 
   useEffect(() => {
     document.title = 'Поиск Вакансий';
-    updateVacancies(state, setVacanciesList);
+    updateVacancies(state, setVacanciesList, setLoading);
   }, [state]);
-
-  if (vacanciesList.length) {
-    return (
-      <main className="main-list">
-        <div className="jobs-list-grid">
-          <FilterComponent onApplyFilter={dispatch} filterInitState={state.filter} />
-          <div className="list-container">
-            <KeywordInputComponent onSearch={dispatch} keywordInitState={state.keyword} />
-            <ul className="list-container">
-              {vacanciesList.map((v) => (
-                <ListItemComponent key={v.id} {...v} isAlone={false} />
-              ))}
-            </ul>
-          </div>
-        </div>
-      </main>
-    );
-  }
 
   return (
     <main className="main-list">
@@ -73,11 +60,24 @@ const JobsListComponent: React.FC = () => {
         <FilterComponent onApplyFilter={dispatch} filterInitState={state.filter} />
         <div className="list-container">
           <KeywordInputComponent onSearch={dispatch} keywordInitState={state.keyword} />
-          <div className="list-container">
-            <EmptyItemComponent />
-          </div>
+          {vacanciesList.length > 0 ? (
+            <ul className="list-container">
+              {vacanciesList.map((v) => (
+                <ListItemComponent key={v.id} {...v} isAlone={false} />
+              ))}
+            </ul>
+          ) : (
+            <div className="list-container">
+              <EmptyItemComponent />
+            </div>
+          )}
         </div>
       </div>
+      {isLoading ? (
+        <div className="overlay-container">
+          <SpinnerComponent />
+        </div>
+      ) : null}
     </main>
   );
 };
